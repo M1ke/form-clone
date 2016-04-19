45396e04992c101399e30697c2546ab6472bce17
m1ke <git@m1ke.me>
2016-04-19 13:21:58 +0100

    Updated form clone

--- form-clone.js
+++ form-clone.js
@@ -1,79 +1,93 @@
 /*@include ../js-utils-jq/input-blank.js */
 
 var formClone = {
-	event: 'form-cloned'
-	,setQuantity: function(num, reduce, item){
-		$(typeof item!='undefined' ? item : '.form-clone:first').formCloneQuantity(num, reduce);
+	event: 'form-cloned',
+	setQuantity: function(num, reduce, item){
+		$(typeof item!=='undefined' ? item : '.form-clone:first').formCloneQuantity(num, reduce);
 	}
 };
 
 $.fn.formCloneQuantity = function(num, reduce){
 	var i = 0;
-	while (this.find('.form-clone-el').length<num && i<num){
-		this.find('.form-clone-add').click();
+	var opts = this.data('form-clone');
+	while (this.children(opts.el).length<num && i<num){
+		this.children('.form-clone-add').click();
 		i++;
+	}
+	if (reduce){
+		i = this.children(opts.el).length;
+		while (this.children(opts.el).length>num && i>0){
+			this.children('.form-clone-remove').last().click();
+			i--;
+		}
 	}
 };
 
 $.fn.formClone = function(options){
-
-	return this.each(function(options){
-		return function(key){
-			var opts = $.extend({
-				all: $(this).data('clone-all') || true
-				,buttonCss: {'float': 'right', clear: 'right'}
-				,buttonText: '+'
-				,el: $(this).data('clone-el') || 'div'
-				,elCss: {overflow: 'hidden'}
-			}, options);
-			if (opts.onClone){
-				$(this).bind('form-cloned', opts.onClone);
+	return this.each(function(key){
+		var opts = $.extend({
+			all: $(this).data('clone-all') || true,
+			allValues: $(this).data('clone-all-values') || false,
+			buttonCss: {'float': 'right', clear: 'right'},
+			buttonText: '+',
+			el: $(this).data('clone-el') || 'div',
+			elCss: {
+				overflow: 'hidden'
 			}
-			var $items = $(this).children(opts.el)
-				,length = $items.length;
-			$items.addClass('form-clone-el').each(function(opts){
-				return function(key){
-					var $formCloneRemoveButton = function(el,buttonCss){
-						return $('<a href="#" class="form-clone-button form-clone-remove">-</a>').data('el', el).formCloneRemove(buttonCss);
-					};
-					if (length!=1){
-						$formCloneRemoveButton(opts.el, opts.buttonCss).insertBefore($(this));
+		}, options);
+
+		if (opts.onClone){
+			$(this).bind('form-cloned', opts.onClone);
+		}
+
+		var $items = $(this).children(opts.el)
+			, length = $items.length;
+
+		$(this).data('form-clone', opts);
+
+		$items.addClass('form-clone-el').each(function(key){
+			function $formCloneRemoveButton(el, buttonCss){
+				return $('<a href="#" class="form-clone-button form-clone-remove">-</a>').data('el', el).formCloneRemove(buttonCss);
+			}
+
+			if (length!==1){
+				$formCloneRemoveButton(opts.el, opts.buttonCss).insertBefore($(this));
+			}
+
+			if (key===(length-1)){
+				$('<a href="#" class="form-clone-button form-clone-add">'+opts.buttonText+'</a>').data(opts).click(function(e){
+					e.preventDefault();
+					var el = $(this).data('el')
+						, $div = $(this).parent().find($(this).data('el')).last()
+						, $cloned;
+
+					if ($(this).prev('.form-clone-remove').length<1){
+						$formCloneRemoveButton(el, $(this).data('buttonCss')).insertBefore($div);
 					}
-					if (key==(length-1)){
-						$('<a href="#" class="form-clone-button form-clone-add">'+opts.buttonText+'</a>').data(opts).click(function(e){
-							e.preventDefault();
-							var el = $(this).data('el')
-								,$div = $(this).parent().find($(this).data('el')).last()
-								,$cloned;
 
-							if ($(this).prev('.form-clone-remove').length<1){
-								$formCloneRemoveButton(el, $(this).data('buttonCss')).insertBefore($div);
-							}
+					$(this).insertAfter($div);
 
-							$(this).insertAfter($div);
+					// Pass in "all" to copy events
+					$cloned = $div.clone($(this).data('all')).insertAfter($(this));
 
-							// Pass in "all" to copy events
-							$cloned = $div.clone($(this).data('all')).insertAfter($(this));
+					$formCloneRemoveButton(el, $(this).data('buttonCss')).insertBefore($(this));
 
-							$formCloneRemoveButton(el, $(this).data('buttonCss')).insertBefore($(this));
-
-							// Set data.all-values to copy values
-							if (!$(this).data('all-values')){
-								$cloned.inputBlankArray().find('input:not([type="submit"])').val('');
-							}
-
-							$(this).parent().trigger(formClone.event);
-
-							if (typeof $(this).data('cloneCallback')=='function'){
-								$(this).data('cloneCallback').call(null, $cloned, $div);
-							}
-						}).css(opts.buttonCss).attr('title', 'Click to add another').insertBefore($(this));
+					// Set data.all-values to copy values
+					if (!$(this).data('all-values')){
+						$cloned.inputBlankArray().find('input:not([type="submit"])').val('');
 					}
-					$(this).css(opts.elCss);
-				};
-			}(opts));
-		};
-	}(options));
+
+					$(this).parent().trigger(formClone.event);
+
+					if (typeof $(this).data('cloneCallback')=='function'){
+						$(this).data('cloneCallback').call(null, $cloned, $div);
+					}
+				}).css(opts.buttonCss).attr('title', 'Click to add another').insertBefore($(this));
+			}
+
+			$(this).css(opts.elCss);
+		});
+	});
 };
 
 $.fn.formCloneInit = function(opts){
@@ -81,18 +95,21 @@ $.fn.formCloneInit = function(opts){
 };
 
 $.fn.formCloneRemove = function(buttonCss){
-	return this.css(buttonCss).attr('title','Click to remove this').click(function(e){
+	return this.css(buttonCss).attr('title', 'Click to remove this').click(function(e){
 		e.preventDefault();
 		var $parent = $(this).parent()
-			,clones = $parent.find($(this).data('el')).length-1;
+			, clones = $parent.find($(this).data('el')).length-1;
+
 		$(this).nextAll($(this).data('el')+':first').remove();
-		if (0==$(this).nextAll($(this).data('el')).length){
+		if (0===$(this).nextAll($(this).data('el')).length){
 			$(this).prevAll($(this).data('el')+':first').before($(this).siblings('.form-clone-add'));
 		}
 		$(this).remove();
+
 		if (clones<2){
 			$parent.find('.form-clone-remove').remove();
 		}
+
 		$parent.trigger(formClone.event);
 	});
 };
